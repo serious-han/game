058e2e25b26c3ecdf9a64f570e4b84b206a88252
jande460 <50678826+jande460@users.noreply.github.com>
2019-05-24 13:34:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var response = prompt('Do you want to play a game (y/n).');
-
+let gameData = [
+  [1,2],
+  [3,4],
+  [5,6]
+];
 
 
 const playGame = () =>
 {
-
+  
 };
 
 
@@ -17,5 +21,7 @@ const playGame = () =>
 
 if(response === 'y' || response === 'Y')
 {
-  alert('ok!')
+  alert('Ok!')
 }
+
+alert(gameData[0][0]);
